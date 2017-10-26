560d096d964d16cb830dbf05dedda449af2da085
Alexpham688 <Alexpham688@gmail.com>
2017-10-25 22:19:17 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = angular.module('myApp',[]);
+app.controller('myCtrl', function($scope){
+  $scope.list = [
+    'wash the car',
+    'clean the house',
+    'walk the dog',
+    'mop the floor',
+    'get the dry clean'
+  ];
+  // $scope.temp = false;
+  //add item
+  $scope.addItem = function(newItem){
+    $scope.temp = false;
+    // $scope.addChore = "";
+    $scope.list.push(newItem)
+    $scope.item = "";
+  };
+  //delete list item
+  $scope.deleteRow = function(item){
+    $scope.list.splice($scope.list.indexOf(item), 1);
+  };
+  //add plural if multiple
+  $scope.plural = function(list){
+    return list.length > 1 || list.length == 0 ? 's' :'';
+  };
+
+  $scope.addTemp = function(item) {
+    if($scope.temp) $scope.list.pop();
+    else if($scope.addName) $scope.temp = true;
+    if($scope.addName)
+    $scope.list.push($scope.addName);
+    else $scope.temp = false;
+    };
+
+});
